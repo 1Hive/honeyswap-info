7f4830105428657dc74dd337d71bfff7fb521d4a
Hernando Guzmán <hernando.guzman93@gmail.com>
2021-05-31 15:38:57 -0300

    Change loader to flying bee

--- src/components/LocalLoader/index.js
+++ src/components/LocalLoader/index.js
@@ -1,45 +1,28 @@
 import React from 'react'
-import styled, { css, keyframes } from 'styled-components'
-// import { useDarkModeManager } from '../../contexts/LocalStorage'
-
-const pulse = keyframes`
-  0% { transform: scale(1); }
-  60% { transform: scale(1.1); }
-  100% { transform: scale(1); }
-`
+import { Lottie } from '@crello/react-lottie'
+import styled from 'styled-components'
+import beeAnimation from '../../assets/lotties/bee-animation.json'
 
 const Wrapper = styled.div`
   pointer-events: none;
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 100%;
+  height: ${(props) => (props.fill ? "100vh" : "180px")};
   width: 100%;
-
-  ${(props) =>
-    props.fill && !props.height
-      ? css`
-          height: 100vh;
-        `
-      : css`
-          height: 180px;
-        `}
-`
-
-const AnimatedImg = styled.div`
-  animation: ${pulse} 800ms linear infinite;
-  & > * {
-    width: 72px;
-  }
 `
 
 const LocalLoader = ({ fill }) => {
-
   return (
     <Wrapper fill={fill}>
-      <AnimatedImg>
-        <img src={require("../../assets/bee.svg")} alt="loading-icon" />
-      </AnimatedImg>
+      <Lottie
+        config={{
+          animationData: beeAnimation,
+          loop: true,
+        }}
+        height={fill ? 100 : 84}
+        width={fill ? 100 : 84}
+      />
     </Wrapper>
   )
 }
